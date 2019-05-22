56b2c7d034febee6e24196c4f868d77763e6e052
magomed mourdalov <magomedmourdalov@gmail.com>
2019-05-22 12:22:20 +0200

    knop toegevoegd

--- VanEeckhauteWolf/Form1.cs
+++ VanEeckhauteWolf/Form1.cs
@@ -16,5 +16,10 @@ namespace VanEeckhauteWolf
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hallo niueuwe knop");
+        }
     }
 }
